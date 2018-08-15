0c2b06d48755e08dc741e7eee546a71aa058c038
Marcisbee <marcisbergmanis@gmail.com>
2018-08-15 18:21:07 +0300

    Adds test

---
+++ src/__tests__/radi-compiler.test.js
@@ -0,0 +1,7 @@
+import radiCompiler from '../radi-compiler';
+
+describe('radi-compiler.js', () => {
+  it('exports function', () => {
+    expect(typeof radiCompiler).toEqual('function');
+  });
+});
